647a32d0e711277e2ce669b18705bbc1f1fed0ca
maxgoover <maxgoover@gmail.com>
2020-05-09 21:32:12 +0300

    Unified authentification part 2

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,9 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'cookieDomain' => '.example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
