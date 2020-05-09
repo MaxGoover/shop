589b25f5ff08f1e4530602c3f77fb991d8c4e9e5
maxgoover <maxgoover@gmail.com>
2020-05-09 21:29:53 +0300

    Unified authentification part 1

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,16 +14,24 @@ return [
     'modules' => [],
     'components' => [
         'request' => [
+            'cookieValidationKey' => $params['cookieValidationKey'],
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'identityCookie' => [
+                'name' => '_identity',
+                'httpOnly' => true,
+                'domain' => $params['cookieDomain'],
+            ],
         ],
         'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
+            'name' => '_session',
+            'cookieParams' => [
+                'domain' => $params['cookieDomain'],
+                'httpOnly' => true,
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
