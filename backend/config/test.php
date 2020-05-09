f0cb8335715d0108f24f0a9460aadf4ef7d0e111
maxgoover <maxgoover@gmail.com>
2020-05-09 21:37:08 +0300

    Unified authentification part 3

--- backend/config/test.php
+++ backend/config/test.php
@@ -1,15 +1,21 @@
 <?php
+
+use yii\helpers\ReplaceArrayValue;
+
 return [
     'id' => 'app-backend-tests',
     'components' => [
         'assetManager' => [
             'basePath' => __DIR__ . '/../web/assets',
         ],
-        'urlManager' => [
-            'showScriptName' => true,
-        ],
         'request' => [
             'cookieValidationKey' => 'test',
+        ],
+        'session' => [
+            'cookieParams' => new ReplaceArrayValue(['httpOnly' => true]),
+        ],
+        'urlManager' => [
+            'showScriptName' => true,
         ],
     ],
 ];
