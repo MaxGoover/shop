48907bfeae430ac5deb87abe61b2750c8987631e
maxgoover <maxgoover@gmail.com>
2020-05-10 13:40:14 +0300

    Login rememberMe = false (by default)

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -41,7 +41,7 @@ $fieldOptions2 = [
 
         <div class="row">
             <div class="col-xs-8">
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= $form->field($model, 'rememberMe')->checkbox(['value' => '0']) ?>
             </div>
             <!-- /.col -->
             <div class="col-xs-4">
