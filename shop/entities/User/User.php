aabed350cb2ed7c7d1445ea803b239f60f6aeb7c
maxgoover <maxgoover@gmail.com>
2020-05-13 15:14:45 +0300

    Renamed user table

--- shop/entities/User/User.php
+++ shop/entities/User/User.php
@@ -46,7 +46,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%users}}';
     }
 
     /**
