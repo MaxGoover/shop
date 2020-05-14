6dbc3c8c81eb58970fb942d64da6483db6028ad3
maxgoover <maxgoover@gmail.com>
2020-05-14 13:54:54 +0300

    Added composite form

--- shop/forms/manage/Shop/BrandForm.php
+++ shop/forms/manage/Shop/BrandForm.php
@@ -3,44 +3,40 @@
 namespace shop\forms\manage\Shop;
 
 use shop\entities\Shop\Brand;
+use shop\forms\CompositeForm;
 use shop\forms\manage\MetaForm;
 use yii\base\Model;
 
 /**
  * @property MetaForm $meta;
  */
-class BrandForm extends Model
+class BrandForm extends CompositeForm
 {
     public $name;
     public $slug;
 
-    private $_meta;
     private $_brand;
+    private $_meta;
 
     public function __construct(Brand $brand = null, $config = [])
     {
         if ($brand) {
             $this->name = $brand->name;
             $this->slug = $brand->slug;
-            $this->_meta = new MetaForm($brand->meta);
             $this->_brand = $brand;
+            $this->_meta = new MetaForm($brand->meta);
+        } else {
+            $this->_meta = new MetaForm();
         }
         parent::__construct($config);
     }
 
-    public function load($data, $formName = null): bool
+    public function internalForms(): array
     {
-        $self = parent::load($data, $formName);
-        $meta = $this->_meta->load($data, $formName ? null : 'meta');
-        return $self && $meta;
+        return ['meta'];
     }
 
-    public function validate($attributeNames = null, $clearErrors = true): bool
-    {
-        $self = parent::validate($attributeNames, $clearErrors);
-        $meta = $this->_meta->validate($attributeNames, $clearErrors);
-        return $self && $meta;
-    }
+    ##########################
 
     public function rules(): array
     {
@@ -50,10 +46,5 @@ class BrandForm extends Model
             ['slug', 'match', 'pattern' => '#^[a-z0-9_-]*$#s'],
             [['name', 'slug'], 'unique', 'targetClass' => Brand::class, 'filter' => $this->_brand ? ['<>', 'id', $this->_brand->id] : null]
         ];
-    }
-
-    public function getMeta(): MetaForm
-    {
-        return $this->_meta;
     }
 }
