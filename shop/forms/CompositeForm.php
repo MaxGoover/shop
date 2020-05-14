dad246de2b74dd3c3d973172a9b65e3a4ea0696a
maxgoover <maxgoover@gmail.com>
2020-05-14 14:02:50 +0300

    Added composite arrays supporting

--- shop/forms/CompositeForm.php
+++ shop/forms/CompositeForm.php
@@ -8,28 +8,45 @@ use yii\helpers\ArrayHelper;
 abstract class CompositeForm extends Model
 {
     /**
-     * @var Model[]
+     * @var Model[]|array[]
      */
     private $forms = [];
 
     abstract protected function internalForms(): array;
 
+    /**
+     * @param array $data
+     * @param null $formName
+     * @return bool
+     */
     public function load($data, $formName = null): bool
     {
         $success = parent::load($data, $formName);
         foreach ($this->forms as $name => $form) {
-            $success = $form->load($data, $formName !== '' ? null : $name) && $success;
-        }
+            if (\is_array($form)) {
+                $success = Model::loadMultiple($form, $data, $formName === null ? null : $name) && $success;
+            } else {
+                $success = $form->load($data, $formName !== '' ? null : $name) && $success;
+            }        }
         return $success;
     }
 
+    /**
+     * @param null $attributeNames
+     * @param bool $clearErrors
+     * @return bool
+     */
     public function validate($attributeNames = null, $clearErrors = true): bool
     {
-        $parentNames = array_filter((array)$attributeNames, 'is_string');
+        $parentNames = $attributeNames !== null ? \array_filter((array)$attributeNames, 'is_string') : null;
         $success = parent::validate($parentNames, $clearErrors);
         foreach ($this->forms as $name => $form) {
-            $innerNames = ArrayHelper::getValue($attributeNames, $name);
-            $success = $form->validate($innerNames, $clearErrors) && $success;
+            if (\is_array($form)) {
+                $success = Model::validateMultiple($form) && $success;
+            } else {
+                $innerNames = $attributeNames !== null ? ArrayHelper::getValue($attributeNames, $name) : null;
+                $success = $form->validate($innerNames ?: null, $clearErrors) && $success;
+            }
         }
         return $success;
     }
