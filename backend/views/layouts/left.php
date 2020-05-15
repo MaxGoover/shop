6e2c34de7513b22ddf9c05c1b83d08b73b29f03a
maxgoover <maxgoover@gmail.com>
2020-05-15 09:03:47 +0300

    Added tags management

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -8,7 +8,7 @@
                 <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Max Zaharov</p>
+                <p>Alexander Pierce</p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
@@ -33,6 +33,7 @@
                     ['label' => 'Management', 'options' => ['class' => 'header']],
                         ['label' => 'Shop', 'icon' => 'folder', 'items' => [
                             ['label' => 'Brands', 'icon' => 'file-o', 'url' => ['/shop/brand/index'], 'active' => $this->context->id == 'shop/brand'],
+                            ['label' => 'Tags', 'icon' => 'file-o', 'url' => ['/shop/tag/index'], 'active' => $this->context->id == 'shop/tag'],
                         ]],
                     ['label' => 'Users', 'icon' => 'user', 'url' => ['/user/index'], 'active' => $this->context->id == 'user/index'],
                 ],
