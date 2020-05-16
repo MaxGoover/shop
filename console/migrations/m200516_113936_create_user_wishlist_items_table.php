b2ef544454a94bfdaa07cefdbdcf46d2adeff7f4
maxgoover <maxgoover@gmail.com>
2020-05-16 14:44:19 +0300

    Added wishlist model

---
+++ console/migrations/m200516_113936_create_user_wishlist_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user_wishlist_items}}`.
+ */
+class m200516_113936_create_user_wishlist_items_table extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%user_wishlist_items}}', [
+            'user_id' => $this->integer()->notNull(),
+            'product_id' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        $this->addPrimaryKey('{{%pk-user_wishlist_items}}', '{{%user_wishlist_items}}', ['user_id', 'product_id']);
+
+        $this->createIndex('{{%idx-user_wishlist_items-user_id}}', '{{%user_wishlist_items}}', 'user_id');
+        $this->createIndex('{{%idx-user_wishlist_items-product_id}}', '{{%user_wishlist_items}}', 'product_id');
+
+        $this->addForeignKey('{{%fk-user_wishlist_items-user_id}}', '{{%user_wishlist_items}}', 'user_id', '{{%users}}', 'id', 'CASCADE', 'RESTRICT');
+        $this->addForeignKey('{{%fk-user_wishlist_items-product_id}}', '{{%user_wishlist_items}}', 'product_id', '{{%shop_products}}', 'id', 'CASCADE', 'RESTRICT');
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%user_wishlist_items}}');
+    }
+}
