87f670fc46fc428c1e2172064dcc7ec09fafb11e
maxgoover <maxgoover@gmail.com>
2020-05-19 19:22:04 +0300

    Refactored shop/services/sms

--- shop/services/sms/LoggedSender.php
+++ shop/services/sms/LoggedSender.php
@@ -6,18 +6,18 @@ use yii\log\Logger;
 
 class LoggedSender implements SmsSender
 {
-    private $next;
-    private $logger;
+    private $_next;
+    private $_logger;
 
     public function __construct(SmsSender $next, Logger $logger)
     {
-        $this->next = $next;
-        $this->logger = $logger;
+        $this->_next = $next;
+        $this->_logger = $logger;
     }
 
     public function send($number, $text): void
     {
-        $this->next->send($number, $text);
-        $this->logger->log('Message to ' . $number . ': ' . $text, Logger::LEVEL_INFO);
+        $this->_next->send($number, $text);
+        $this->_logger->log('Message to ' . $number . ': ' . $text, Logger::LEVEL_INFO);
     }
 }
