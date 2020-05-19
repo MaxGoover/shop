bc6872ddd3b9dc66a295acc0745fb090017cc915
maxgoover <maxgoover@gmail.com>
2020-05-19 10:13:44 +0300

    Refactored backend/widgets

--- backend/widgets/grid/RoleColumn.php
+++ backend/widgets/grid/RoleColumn.php
@@ -13,7 +13,7 @@ class RoleColumn extends DataColumn
     protected function renderDataCellContent($model, $key, $index): string
     {
         $roles = Yii::$app->authManager->getRolesByUser($model->id);
-        return $roles === [] ? $this->grid->emptyCell : implode(', ', array_map(function (Item $role) {
+        return $roles === [] ? $this->grid->emptyCell : \implode(', ', \array_map(function (Item $role) {
             return $this->getRoleLabel($role);
         }, $roles));
     }
