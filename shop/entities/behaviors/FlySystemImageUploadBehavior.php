c6d7aa3aa15468524eb9c0999ac75e75f7a17030
maxgoover <maxgoover@gmail.com>
2020-05-19 12:32:46 +0300

    Refactored shop/entities/behaviors

--- shop/entities/behaviors/FlySystemImageUploadBehavior.php
+++ shop/entities/behaviors/FlySystemImageUploadBehavior.php
@@ -4,36 +4,35 @@ namespace shop\entities\behaviors;
 
 use League\Flysystem\Filesystem;
 use PHPThumb\GD;
-use yii\base\Exception;
 use yii\helpers\FileHelper;
 use yii\web\UploadedFile;
 use yiidreamteam\upload\ImageUploadBehavior;
 
 class FlySystemImageUploadBehavior extends ImageUploadBehavior
 {
-    private $filesystem;
+    private $_filesystem;
 
     public function __construct(Filesystem $filesystem, $config = [])
     {
         parent::__construct($config);
-        $this->filesystem = $filesystem;
-    }
-
-    public function cleanFiles(): void
-    {
-        $this->filesystem->delete($this->resolvePath($this->filePath));
-        foreach (array_keys($this->thumbs) as $profile) {
-            $this->filesystem->delete($this->getThumbFilePath($this->attribute, $profile));
-        }
+        $this->_filesystem = $filesystem;
     }
 
     public function afterSave(): void
     {
         if ($this->file instanceof UploadedFile) {
             $path = $this->getUploadedFilePath($this->attribute);
-            $this->filesystem->createDir(pathinfo($path, PATHINFO_DIRNAME));
-            $this->filesystem->put($path, file_get_contents($this->file->tempName));
+            $this->_filesystem->createDir(\pathinfo($path, PATHINFO_DIRNAME));
+            $this->_filesystem->put($path, \file_get_contents($this->file->tempName));
             $this->owner->trigger(static::EVENT_AFTER_FILE_SAVE);
+        }
+    }
+
+    public function cleanFiles(): void
+    {
+        $this->_filesystem->delete($this->resolvePath($this->filePath));
+        foreach (\array_keys($this->thumbs) as $profile) {
+            $this->_filesystem->delete($this->getThumbFilePath($this->attribute, $profile));
         }
     }
 
@@ -42,10 +41,10 @@ class FlySystemImageUploadBehavior extends ImageUploadBehavior
         $path = $this->getUploadedFilePath($this->attribute);
         foreach ($this->thumbs as $profile => $config) {
             $thumbPath = static::getThumbFilePath($this->attribute, $profile);
-            if ($this->filesystem->has($path) && !$this->filesystem->has($thumbPath)) {
+            if ($this->_filesystem->has($path) && !$this->_filesystem->has($thumbPath)) {
 
                 // setup image processor function
-                if (isset($config['processor']) && is_callable($config['processor'])) {
+                if (isset($config['processor']) && \is_callable($config['processor'])) {
                     $processor = $config['processor'];
                     unset($config['processor']);
                 } else {
@@ -55,14 +54,14 @@ class FlySystemImageUploadBehavior extends ImageUploadBehavior
                 }
 
                 $tmpPath = $this->getTempPath($thumbPath);
-                FileHelper::createDirectory(pathinfo($tmpPath, PATHINFO_DIRNAME), 0775, true);
-                file_put_contents($tmpPath, $this->filesystem->get($path));
+                FileHelper::createDirectory(\pathinfo($tmpPath, PATHINFO_DIRNAME), 0775, true);
+                \file_put_contents($tmpPath, $this->_filesystem->get($path));
                 $thumb = new GD($tmpPath, $config);
-                call_user_func($processor, $thumb, $this->attribute);
-                FileHelper::createDirectory(pathinfo($thumbPath, PATHINFO_DIRNAME), 0775, true);
+                \call_user_func($processor, $thumb, $this->attribute);
+                FileHelper::createDirectory(\pathinfo($thumbPath, PATHINFO_DIRNAME), 0775, true);
                 $thumb->save($tmpPath);
-                $this->filesystem->createDir(pathinfo($thumbPath, PATHINFO_DIRNAME));
-                $this->filesystem->put($thumbPath, file_get_contents($tmpPath));
+                $this->_filesystem->createDir(\pathinfo($thumbPath, PATHINFO_DIRNAME));
+                $this->_filesystem->put($thumbPath, \file_get_contents($tmpPath));
             }
         }
     }
@@ -73,6 +72,6 @@ class FlySystemImageUploadBehavior extends ImageUploadBehavior
      */
     private function getTempPath($path): string
     {
-        return sys_get_temp_dir() . $path;
+        return \sys_get_temp_dir() . $path;
     }
 }
