0dbf67b90943659f18200e4256d0902f7caa8152
maxgoover <maxgoover@gmail.com>
2020-05-19 11:56:49 +0300

    Refactored shop/cart

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -81,8 +81,8 @@ class SetUp implements BootstrapInterface
 
         $container->setSingleton(Cart::class, function () use ($app) {
             return new Cart(
-                new HybridStorage($app->get('user'), 'cart', 3600 * 24, $app->db),
-                new DynamicCost(new SimpleCost())
+                new DynamicCost(new SimpleCost()),
+                new HybridStorage($app->get('user'), 'cart', 3600 * 24, $app->db)
             );
         });
 
--- shop/cart/Cart.php
+++ shop/cart/Cart.php
@@ -8,17 +8,50 @@ use shop\cart\storage\StorageInterface;
 
 class Cart
 {
-    private $storage;
-    private $calculator;
+    private $_calculator;
+    private $_storage;
+
     /**
      * @var CartItem[]
      * */
-    private $items;
+    private $_items;
 
-    public function __construct(StorageInterface $storage, CalculatorInterface $calculator)
+    public function __construct(CalculatorInterface $calculator, StorageInterface $storage)
     {
-        $this->storage = $storage;
-        $this->calculator = $calculator;
+        $this->_calculator = $calculator;
+        $this->_storage = $storage;
+    }
+
+    public function add(CartItem $item): void
+    {
+        $this->_loadItems();
+        foreach ($this->_items as $i => $current) {
+            if ($current->getId() == $item->getId()) {
+                $this->_items[$i] = $current->plus($item->getQuantity());
+                $this->_saveItems();
+                return;
+            }
+        }
+        $this->_items[] = $item;
+        $this->saveItems();
+    }
+
+    public function clear(): void
+    {
+        $this->_items = [];
+        $this->_saveItems();
+    }
+
+    public function getAmount(): int
+    {
+        $this->_loadItems();
+        return count($this->_items);
+    }
+
+    public function getCost(): Cost
+    {
+        $this->_loadItems();
+        return $this->_calculator->getCost($this->_items);
     }
 
     /**
@@ -26,85 +59,53 @@ class Cart
      */
     public function getItems(): array
     {
-        $this->loadItems();
-        return $this->items;
-    }
-
-    public function getAmount(): int
-    {
-        $this->loadItems();
-        return count($this->items);
-    }
-
-    public function add(CartItem $item): void
-    {
-        $this->loadItems();
-        foreach ($this->items as $i => $current) {
-            if ($current->getId() == $item->getId()) {
-                $this->items[$i] = $current->plus($item->getQuantity());
-                $this->saveItems();
-                return;
-            }
-        }
-        $this->items[] = $item;
-        $this->saveItems();
-    }
-
-    public function set($id, $quantity): void
-    {
-        $this->loadItems();
-        foreach ($this->items as $i => $current) {
-            if ($current->getId() == $id) {
-                $this->items[$i] = $current->changeQuantity($quantity);
-                $this->saveItems();
-                return;
-            }
-        }
-        throw new \DomainException('Item is not found.');
-    }
-
-    public function remove($id): void
-    {
-        $this->loadItems();
-        foreach ($this->items as $i => $current) {
-            if ($current->getId() == $id) {
-                unset($this->items[$i]);
-                $this->saveItems();
-                return;
-            }
-        }
-        throw new \DomainException('Item is not found.');
-    }
-
-    public function clear(): void
-    {
-        $this->items = [];
-        $this->saveItems();
-    }
-
-    public function getCost(): Cost
-    {
-        $this->loadItems();
-        return $this->calculator->getCost($this->items);
+        $this->_loadItems();
+        return $this->_items;
     }
 
     public function getWeight(): int
     {
-        $this->loadItems();
+        $this->_loadItems();
         return array_sum(array_map(function (CartItem $item) {
             return $item->getWeight();
-        }, $this->items));
+        }, $this->_items));
     }
 
-    private function loadItems(): void
+    public function remove($id): void
     {
-        if ($this->items === null) {
-            $this->items = $this->storage->load();
+        $this->_loadItems();
+        foreach ($this->_items as $i => $current) {
+            if ($current->getId() == $id) {
+                unset($this->_items[$i]);
+                $this->_saveItems();
+                return;
+            }
+        }
+        throw new \DomainException('Item is not found.');
+    }
+
+    public function set($id, $quantity): void
+    {
+        $this->_loadItems();
+        foreach ($this->_items as $i => $current) {
+            if ($current->getId() == $id) {
+                $this->_items[$i] = $current->changeQuantity($quantity);
+                $this->_saveItems();
+                return;
+            }
+        }
+        throw new \DomainException('Item is not found.');
+    }
+
+    private function _loadItems(): void
+    {
+        if ($this->_items === null) {
+            $this->_items = $this->_storage->load();
         }
     }
 
-    private function saveItems(): void
+    private function _saveItems(): void
     {
-        $this->storage->save($this->items);
+        $this->_storage->save($this->_items);
     }
 } 
